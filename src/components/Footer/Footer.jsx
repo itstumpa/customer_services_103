8ce52d367dda6d738c54c4c5f1c2e76be9c4f745
tumpa <tumpadas3494@gmail.com>
2025-09-27 00:17:31 +0600

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // React component to render icons
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+
 
 const Footer = () => {
   return (
-    <div className=' bg-black h-[400px] mt-16 px-8 py-20 '>
+    <div className=' bg-black h-auto mt-16 px-8 pt-20 '>
       <div className='max-w-[1200px] mx-auto flex  justify-between text-white' >
 <div>
       <h1 className='font-bold text-2xl mb-2'>CS — Ticket System</h1>
@@ -10,31 +16,45 @@ const Footer = () => {
 </div>
 
 <div>
-<h1 className='font-bold text-2xl mb-2'>Company</h1>
-      <p>urf</p>
-      <p>hjt</p>
-      <p>fhygftr</p>
-</div>
-<div>
-      <h1 className='font-bold text-2xl mb-2'>Services</h1>
-      <p>urf</p>
-      <p>hjt</p>
-      <p>fhygftr</p>
-</div>
-<div>
-      <h1 className='font-bold text-2xl mb-2'>Information</h1>
-      <p>urf</p>
-      <p>hjt</p>
-      <p>fhygftr</p>
-</div>
-<div>
-      <h1 className='font-bold text-2xl mb-2'>Social links</h1>
-      <p>urf</p>
-      <p>hjt</p>
-      <p>fhygftr</p>
-</div>
+      <h1 className='font-semibold text-xl mb-2'>Company</h1>
+      <div className='text-[#777777] spacey-y-8'>
 
+      <p>About Us</p>
+      <p>Our Mission</p>
+      <p>Contact Saled</p>
       </div>
+</div>
+<div>
+      <h1 className='font-semibold text-xl mb-2'>Services</h1>
+      <div className='text-[#777777] spacey-y-8'>
+
+      <p>Products & Services</p>
+      <p>Customer Stories</p>
+      <p>Download Apps</p>
+      </div>
+</div>
+<div>
+      <h1 className='font-semibold text-xl mb-2'>Information</h1>
+      <div className='text-[#777777] space-y-1'>
+
+      <p>Privacy Policy</p>
+      <p>Terms & Conditions</p>
+      <p>Join Us</p>
+      </div>
+</div>
+<div>
+      <h1 className='font-semibold text-xl mb-2'>Social links</h1>
+      <div className='text-[#777777] space-y-1'>
+
+      <p><FontAwesomeIcon icon={faCircleXmark} /> @CS — Ticket System</p>
+      <p><FontAwesomeIcon icon={faLinkedin} /> @CS — Ticket System</p>
+      <p><FontAwesomeIcon icon={faFacebook} /> @CS — Ticket System</p>
+      <p><FontAwesomeIcon icon={faEnvelope} /> support@cst.com</p>
+      </div>
+</div>
+      </div>
+<div className='text-white text-center h-[60px] border-t-2 flex items-center justify-center border-[#777777] w-[1200px] mx-auto mt-12'>
+      <p className='text-sm'>© 2025 CS — Ticket System. All rights reserved.</p></div>
     </div>
   )
 }
